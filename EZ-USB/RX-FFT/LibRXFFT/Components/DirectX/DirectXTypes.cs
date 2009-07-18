5f969937efaaf498ce45e95db828c3a91f17f76d
geggo <geggo@localhost>
2009-07-18 18:44:24 +0000

    added new files

---
+++ EZ-USB/RX-FFT/LibRXFFT/Components/DirectX/DirectXTypes.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SlimDX;
+using System.Runtime.InteropServices;
+
+namespace LibRXFFT.Components.DirectX
+{
+    public enum eUserAction
+    {
+        None,
+        YPos,
+        XPos,
+        YZoom,
+        XZoom,
+        YOffset,
+        XOffset,
+        UserCallback
+    }
+
+    public enum eUserEvent
+    {
+        None,
+        MousePosX,
+        MousePosY,
+        MouseDragX,
+        MouseDragXShift,
+        MouseDragXAlt,
+        MouseDragXControl,
+        MouseDragY,
+        MouseDragYShift,
+        MouseDragYAlt,
+        MouseDragYControl,
+        MouseWheel,
+        MouseWheelShift,
+        MouseWheelAlt,
+        MouseWheelControl,
+        RenderOverlay
+    }
+
+    public struct Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Vertex
+    {
+        public Vector4 PositionRhw;
+        public uint Color;
+    }
+
+    public delegate void UserEventCallbackDelegate(eUserEvent evt, double delta);
+}
---
+++ EZ-USB/RX-FFT/LibRXFFT/Libraries/SignalProcessing/DBTools.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibRXFFT.Libraries.SignalProcessing
+{
+    public class DBTools
+    {
+        public static double SampleTodB(double sampleValue)
+        {
+            return 20 * Math.Log10(sampleValue);
+        }
+
+        public static double SampleFromdB(double dBValue)
+        {
+            return Math.Pow(10, (dBValue/20));
+        }
+
+        public static double SquaredSampleTodB(double sampleValue)
+        {
+            return 10 * Math.Log10(sampleValue);
+        }
+
+        public static double SquaredSampleFromdB(double dBValue)
+        {
+            return Math.Pow(10, (dBValue / 10));
+        }
+    }
+}

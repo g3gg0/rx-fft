87a5f9658c7c111f2af770b12761c38a2705e141
geggo <geggo@localhost>
2009-12-12 01:29:36 +0000

    restructured namespace

--- EZ-USB/RX-FFT/RX-FFT/DeviceControls/DeviceControl.cs
+++ EZ-USB/RX-FFT/RX-FFT/DeviceControls/DeviceControl.cs
@@ -5,15 +5,13 @@ using System.Text;
 using LibRXFFT.Libraries.USB_RX.Tuners;
 using LibRXFFT.Libraries.SampleSources;
 
-namespace RX_FFT
+namespace RX_FFT.DeviceControls
 {
     public interface DeviceControl : DigitalTuner
     {
         int SamplesPerBlock { get; set ;}
         SampleSource SampleSource { get; }
         bool Connected { get; }
-
-
 
         void Close();
         void StartRead();

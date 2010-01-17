23114549fb2f34fdeadf4745826e2f5abc6588e6
geggo <geggo@localhost>
2010-01-17 13:28:45 +0000

    restructured GSM stuff

--- EZ-USB/RX-FFT/GaussGenerator/GaussGenerator.cs
+++ EZ-USB/RX-FFT/GaussGenerator/GaussGenerator.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 using LibRXFFT.Libraries;
-using LibRXFFT.Libraries.GMSK;
+
 using LibRXFFT.Libraries.ShmemChain;
 using LibRXFFT.Libraries.SignalProcessing;
 

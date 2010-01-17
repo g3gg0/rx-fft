1432221123731624f83bbce880bc699f3e3c6201
geggo <geggo@localhost>
2010-01-17 13:33:27 +0000

    added RX-Oscilloscope added RX-Setup

---
+++ EZ-USB/RX-FFT/RX-Oscilloscope/Components/Oscilloscope.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibRXFFT.Components.DirectX;
+using LibRXFFT.Libraries.Misc;
+using LibRXFFT.Libraries.Filters;
+using LibRXFFT.Libraries.SignalProcessing;
+
+namespace RX_Oscilloscope.Components
+{
+    public partial class Oscilloscope : UserControl
+    {
+        private bool TriggerRising = false;
+        private bool TriggerFalling = false;
+        private double TriggerLevel = 0.0d;
+        private double LastLevel = 0.0d;
+
+        private int TriggeredSamples = 0;
+        private int SamplesTotal = 10000;
+        private int SamplesPreTrigger = 0;
+        private bool Triggered = false;
+
+        /* when this is set, force waveform display to draw all samples */
+        private bool ForceShowSamples = false;
+
+        private LabelledLine TriggerLevelBar = new LabelledLine("Trig lvl", -1000, Color.BlueViolet);
+        private LabelledLine TriggerSampleBar = new LabelledLine("Trig", -1000, Color.Yellow);
+
+        private IIRFilter LowPass = null;
+
+        public Oscilloscope()
+        {
+            InitializeComponent();
+
+            cmbLowPass.SelectedIndex = 0;
+
+            waveForm.KeepText = true;
+            waveForm.MaxSamples = SamplesTotal;
+            waveForm.LabelledHorLines.AddLast(TriggerLevelBar);
+            waveForm.LabelledVertLines.AddLast(TriggerSampleBar);
+        }
+
+        public double SamplingRate
+        {
+            get
+            {
+                return waveForm.SamplingRate;
+            }
+
+            set
+            {
+                waveForm.SamplingRate = value;
+
+                try
+                {
+                    BeginInvoke(new MethodInvoker(delegate()
+                    {
+                        txtSamplingRate.Frequency = (long)value;
+                        UpdateSampleTimes();
+                    }));
+                }
+                catch (Exception)
+                {}
+            }
+        }
+
+        private double TriggerSamplePos
+        {
+            get
+            {
+                return TriggerSampleBar.Position;
+            }
+            set
+            {
+            }
+        }
+
+        internal void Process(double I, double Q)
+        {
+            double level;
+
+            if (LowPass != null)
+            {
+                level = Math.Sqrt(I * I + Q * Q);
+                level = LowPass.ProcessSample(level);
+                level = DBTools.SampleTodB(level);
+            }
+            else
+            {
+                level = I * I + Q * Q;
+                level = DBTools.SquaredSampleTodB(level);
+            }
+
+
+            if (!Triggered)
+            {
+                if (TriggerRising)
+                {
+                    bool lastOver = LastLevel > TriggerLevel;
+                    bool nowOver = level > TriggerLevel;
+
+                    if (!lastOver && nowOver)
+                    {
+                        TriggeredSamples = 0;
+                        Triggered = true;
+                        waveForm.MainText = "Triggered!";
+                    }
+                }
+
+                if (TriggerFalling)
+                {
+                    bool lastOver = LastLevel > TriggerLevel;
+                    bool nowOver = level > TriggerLevel;
+
+                    if (lastOver && !nowOver)
+                    {
+                        TriggeredSamples = 0;
+                        Triggered = true;
+                        waveForm.MainText = "Triggered!";
+                    }
+                }
+
+                LastLevel = level;
+                waveForm.ProcessData(level, ForceShowSamples);
+            }
+            else
+            {
+                if (TriggeredSamples < SamplesTotal / 2 - SamplesPreTrigger)
+                {
+                    waveForm.ProcessData(level, Triggered);
+                    TriggeredSamples++;
+                }
+            }
+        }
+
+        private void txtSamplingRate_FrequencyChanged(object sender, System.EventArgs e)
+        {
+            if (SamplingRate != txtSamplingRate.Frequency)
+            {
+                SamplingRate = txtSamplingRate.Frequency;
+            }
+        }
+
+
+        private void txtBufferTime_ValueChanged(object sender, System.EventArgs e)
+        {
+            SamplesTotal = (int)txtBufferTime.Value;
+            waveForm.MaxSamples = SamplesTotal;
+
+            UpdateSampleTimes();
+        }
+
+        private void txtPreTrigSamples_ValueChanged(object sender, System.EventArgs e)
+        {
+            SamplesPreTrigger = (int)txtPreTrigSamples.Value;
+
+            UpdateSampleTimes();
+            UpdateTriggerBars();
+        }
+
+        private void txtTriggerLevel_ValueChanged(object sender, System.EventArgs e)
+        {
+            TriggerLevel = (double)txtTriggerLevel.Value;
+
+            UpdateTriggerBars();
+        }
+
+        private void chkTriggerRising_CheckedChanged(object sender, EventArgs e)
+        {
+            TriggerRising = chkTriggerRising.Checked;
+
+            UpdateTriggerBars();
+        }
+
+        private void chkTriggerFalling_CheckedChanged(object sender, EventArgs e)
+        {
+            Triggered = false;
+            TriggerFalling = chkTriggerFalling.Checked;
+
+            UpdateTriggerBars();
+        }
+
+        private void UpdateSampleTimes()
+        {
+            double rate = waveForm.SamplingRate;
+            if (rate != 0)
+            {
+                lblBufferTime.Text = FrequencyFormatter.TimeToString(SamplesTotal / rate);
+                lblPreTrigTime.Text = FrequencyFormatter.TimeToString(SamplesPreTrigger / rate);
+            }
+
+            ForceShowSamples = SamplesPreTrigger > (SamplesTotal / 2);
+        }
+
+        private void UpdateTriggerBars()
+        {
+            if (TriggerRising || TriggerFalling)
+            {
+                waveForm.MainText = "Armed...";
+                TriggerLevelBar.Position = TriggerLevel;
+                TriggerSampleBar.Position = SamplesTotal / 2 + SamplesPreTrigger;
+                waveForm.UpdateOverlays = true;
+            }
+            else
+            {
+                waveForm.MainText = "";
+                TriggerLevelBar.Position = -1000;
+                TriggerSampleBar.Position = -1000;
+                waveForm.UpdateOverlays = true;
+            }
+
+            Triggered = false;
+        }
+
+        private void cmbLowPass_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cmbLowPass.SelectedIndex)
+            {
+                case 0:
+                    LowPass = null;
+                    break;
+                case 1:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_2);
+                    break;
+                case 2:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_4);
+                    break;
+                case 3:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_8);
+                    break;
+                case 4:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_16);
+                    break;
+                case 5:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_32);
+                    break;
+                case 6:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_64);
+                    break;
+                case 7:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_128);
+                    break;
+                case 8:
+                    LowPass = new IIRFilter(IIRCoefficients.IIRLowPass_256);
+                    break;
+            }
+        }
+    }
+}
---
+++ EZ-USB/RX-FFT/RX-Oscilloscope/RXOscilloscope.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibRXFFT.Libraries.SampleSources;
+using System.Threading;
+using LibRXFFT.Libraries.ShmemChain;
+
+namespace RX_Oscilloscope
+{
+    public partial class RXOscilloscope : Form
+    {
+        private SampleSource SampleSource;
+        private Thread ProcessThread;
+        private bool Processing;
+        public int SharedMemoryChannel = 0;
+
+        public RXOscilloscope()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnClosing(CancelEventArgs e)
+        {
+            CloseSource();
+
+            e.Cancel = false;
+            base.OnClosing(e);
+        }
+
+        public void CloseSource()
+        {
+            if (ProcessThread != null)
+            {
+                Processing = false;
+                ProcessThread.Join(500);
+                ProcessThread.Abort();
+                ProcessThread = null;
+            }
+            if (SampleSource != null)
+            {
+                SampleSource.Close();
+                SampleSource = null;
+            }
+        }
+
+        void SampleSource_SamplingRateChanged(object sender, EventArgs e)
+        {
+            scope.SamplingRate = SampleSource.OutputSamplingRate;
+
+            SampleSource.SamplesPerBlock = (int)Math.Min(1024, SampleSource.OutputSamplingRate / 50);
+        }
+
+        private void ProcessMain()
+        {
+            while (Processing)
+            {
+                if (SampleSource.Read())
+                {
+                    for (int pos = 0; pos < SampleSource.SamplesRead; pos++)
+                    {
+                        double I = SampleSource.SourceSamplesI[pos];
+                        double Q = SampleSource.SourceSamplesQ[pos];
+
+                        scope.Process(I, Q);
+                    }
+                }
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (SampleSource != null)
+            {
+                CloseSource();
+                btnOpen.Text = "Open";
+            }
+            else
+            {
+                ContextMenu menu = new ContextMenu();
+
+                menu.MenuItems.Add(new MenuItem("Shared Memory", new EventHandler(btnOpen_SharedMemory)));
+                btnOpen.ContextMenu = menu;
+                btnOpen.ContextMenu.Show(btnOpen, new System.Drawing.Point(10, 10));
+            }
+        }
+
+        public void OpenSharedMem(int srcChan)
+        {
+            SampleSource = new ShmemSampleSource("RX-Oscilloscope", srcChan, 1, 0);
+
+            SampleSource.DataFormat = LibRXFFT.Libraries.ByteUtil.eSampleFormat.Direct16BitIQFixedPoint;
+            SampleSource.SamplingRateChanged += new EventHandler(SampleSource_SamplingRateChanged);
+
+            Processing = true;
+            ProcessThread = new Thread(ProcessMain);
+            ProcessThread.Start();
+
+            btnOpen.Text = "Close";
+        }
+
+        private MenuItem btnOpen_SharedMemoryCreateMenuItem(string name, int srcChan)
+        {
+            MenuItem item;
+
+            if (srcChan < 0)
+            {
+                item = new MenuItem("No data from <" + name + ">");
+                item.Enabled = false;
+            }
+            else
+            {
+                item = new MenuItem("Channel " + srcChan + " from <" + name + ">",
+                new EventHandler(delegate(object sender, EventArgs e)
+                {
+                    OpenSharedMem(srcChan);
+                }));
+            }
+
+            return item;
+        }
+
+        private void btnOpen_SharedMemory(object sender, EventArgs e)
+        {
+            ContextMenu menu = new ContextMenu();
+            NodeInfo[] infos = SharedMem.GetNodeInfos();
+
+            foreach (NodeInfo info in infos)
+            {
+                MenuItem item = btnOpen_SharedMemoryCreateMenuItem(info.name, info.dstChan);
+                menu.MenuItems.Add(item);
+            }
+
+            if (infos.Length == 0)
+            {
+                MenuItem item = new MenuItem("(No nodes found)");
+                item.Enabled = false;
+                menu.MenuItems.Add(item);
+            }
+
+            btnOpen.ContextMenu = menu;
+            btnOpen.ContextMenu.Show(btnOpen, new System.Drawing.Point(10, 10));
+        }
+    }
+}

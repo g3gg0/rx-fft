affc546ba2e1c888ed74af7f47908f145561ba54
geggo <geggo@localhost>
2010-05-08 19:54:07 +0000

    added setup and tests for USB-RX

---
+++ EZ-USB/RX-FFT/USB-RX Console/USBRXConsole.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace USB_RX_Console
+{
+    partial class USBRXConsole
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.frequencySelector1 = new LibRXFFT.Components.GDI.FrequencySelector();
+            this.SuspendLayout();
+            // 
+            // frequencySelector1
+            // 
+            this.frequencySelector1.Location = new System.Drawing.Point(108, 194);
+            this.frequencySelector1.Name = "frequencySelector1";
+            this.frequencySelector1.Size = new System.Drawing.Size(156, 20);
+            this.frequencySelector1.TabIndex = 0;
+            // 
+            // USBRXConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(673, 447);
+            this.Controls.Add(this.frequencySelector1);
+            this.Name = "USBRXConsole";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LibRXFFT.Components.GDI.FrequencySelector frequencySelector1;
+
+    }
+}
+
---
+++ EZ-USB/RX-FFT/USB-RX Console/USBRXConsole.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using LibRXFFT.Libraries.USB_RX.Devices;
+
+namespace USB_RX_Console
+{
+    public partial class USBRXConsole : Form
+    {
+        public USBRXConsole()
+        {
+            InitializeComponent();
+        }
+
+        private string ByteArrayToString(byte[] arr)
+        {
+            System.Text.ASCIIEncoding enc = new System.Text.ASCIIEncoding();
+            return enc.GetString(arr);
+        }
+
+    }
+}
